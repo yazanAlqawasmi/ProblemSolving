905bcf67e51103415c4f095c384dcb6db22a0215
YAZAN <yazanalqawasmi3@gmil.com>
2022-09-13 03:45:20 -1100

    calculator

---
+++ mailPriblem/mail.py
@@ -0,0 +1,70 @@
+# def emailChanger (nam1,nam2):
+#     match tybe :
+#     nam1=input("num1")
+#     nam2=input("nam2")
+#         case "+" :
+# print(nam1+nam2)
+#  ==================================
+#
+# def email (nam1,nam2,type):
+#     match type :
+#
+#         case "+" :
+#             print(float(nam1)+float(nam2))
+#         case "-" :
+#             print(float(nam1)-float(nam2))
+#         case("*"):
+#             print(float(nam1)*float(nam2))
+#         case"/" :
+#             print(float(nam1)/float(nam2))
+# type= input("tyb : ")
+# nam1= input("print nam1 : ")
+# nam2 = input("print nam2 : ")
+# email(nam1,nam2,type)
+#
+# # ==========================
+# def gmail (num1,num2,type):
+#     match type :
+#         case"-":
+#             print(float(num1)-float(num2))
+#         case"+":
+#              print(float(num1) + float(num2))
+#         case"*":
+#              print(float(num1) * float(num2))
+#         case"/":
+#              print(float(num1) / float(num2))
+# type = input("typy : ")
+# num1 = input("nam1 : ")
+# num2 = input("nam2 : ")
+# gmail(num1,num2,type)
+# ============================
+def yahoo (num1,num2,type):
+    match type:
+        case"+":
+            print(float(num1)+float(num2))
+        case("-"):
+            print(float(num1) - float(num2))
+        case("*"):
+            print(float(num1) * float(num2))
+        case("/"):
+            print(floa(num1) /float(num2))
+type = input("typy : ")
+num1 = input("num1 : ")
+num2 = input("num2 : ")
+yahoo(num1,num2,type)
+# =========================================
+def ho (num1,num2,type):
+    match type:
+        case"+":
+            print(float(num1)+float(num2))
+        case("-"):
+            print(float(num1)-float(num2))
+        case("*"):
+             print(float(num1)*float(num2))
+        case("/"):
+            print(float(num1)/float(num2))
+type = input("type : ")
+num1 = input("num1 : ")
+num2 = input("num2 : ")
+ho(num1,num2,type)
+
